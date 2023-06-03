4f8d3b1ac384f66282fabecb8b7c80ba35ba44af
Steeven Ortega Cardona <steevenortega@gmail.com>
2023-06-02 21:54:52 -0500

    Puntos 1 y 2 del paquete de ejercicios while

--- EstructurasControlWhile/script.js
+++ EstructurasControlWhile/script.js
@@ -1,15 +1,51 @@
 // 1. Crea un programa que pregunte al usuario un número. Mostrar los números que son múltiplos de 5 desde 1 hasta el número introducido por el usuario.
 
+// let numero = parseInt(prompt("ingrese el numero maximo a encontrar multiplos de 5"));
+// // let numero = 5
+// console.log(`Los números múltiplos de 5 hasta" ${numero}, son:`);
 
-
+// let i = 1;
+// if(i<=numero){
+//     while ( i <= numero) {
+//         if (i % 5 === 0) {
+//           console.log(i);
+//           i = i+1;
+//         }
+// else{
+//     i = i+1;   
+// }
+// }
+// }
 
 
 // 2. Crea un programa que solicite al usuario 2 números entre 1 y 50. Posteriormente mostrar en consola los números del 1 hasta el 50, pero añadir
 //  el mensaje “¡Lotería!” solo al mostrar los números indicados por el usuario.
 
 
+// let numero1 = parseInt(prompt("Ingrese el primer número (entre 1 y 50):"));
+// let numero2 = parseInt(prompt("Ingrese el segundo número (entre 1 y 50):"));
+// // let numero1 = 8;
+// // let numero2 = 12;
 
+// console.log("Los números del 1 al 50:");
 
+// let i = 0;
+// if (i<=51){
+//     while(i <= 50){
+//         if (i === numero1 || i === numero2) {
+//             console.log(i + " ¡Lotería!");
+//             i = i+1;  
+//           } else {
+//             console.log(i);
+//             i = i+1;          
+//           }    
+          
+//         }    
+//     }
+//     else
+//     {
+//         console.log('termino el proceso');  
+//     }
 
 
 // 3. Crea un programa que solicite al usuario números, si lo que este introduce es un número guardarlo en un arreglo. Para terminar el capturar el usuario 
